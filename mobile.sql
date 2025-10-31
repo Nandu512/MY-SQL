333b6d169faeea64ed0372aceb67b5dd64d269f4
Abhinandan.M <abhiabhinandan242@gmail.com>
2025-10-31 07:58:37 +0530

    practise

---
+++ mobile.sql
@@ -0,0 +1,13 @@
+INSERT INTO mobile (id, name, city, score, bonus, challenge) VALUES
+(1, 'Raj', 'Chennai', 88, 5, 'Fitness'),
+(2, 'Anu', 'Mumbai', 91, NULL, 'Diet'),
+(3, 'Ravi', 'Chennai', 78, 3, 'Fitness'),
+(4, 'Meena', 'Delhi', 82, NULL, 'Diet'),
+(5, 'Farah', 'Mumbai', 95, 4, 'Fitness'),
+(6, 'Kiran', 'Pune', 70, NULL, 'Yoga'),
+(7, 'Latha', 'Pune', 87, NULL, 'Fitness');
+SELECT*FROM mobile
+WHERE score>(SELECT AVG(score)FROM mobile);
+SELECT name FROM mobile
+WHERE challenge=(SELECT challenge FROM mobile 
+                 WHERE name='Farah')
